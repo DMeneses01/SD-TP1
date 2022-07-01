85ce8a2dcb17245a7ba73410703499daed0e86d1
d_meneses <duarte.emanuel.meneses@gmail.com>
2022-07-01 16:54:29 +0100

    code

---
+++ ProjectIntelli/src/client/Return_c.java
@@ -0,0 +1,21 @@
+package client;
+
+public class Return_c {
+
+    public String port1;
+    public String hostname1;
+    public String hostname2;
+    public int serversocket1;
+    public String port2;
+    public int serversocket2;
+
+
+    public Return_c(String port1, String hostname1, String hostname2, int serversocket1, String port2, int serversocket2){
+        this.port1 = port1;
+        this.hostname1 = hostname1;
+        this.hostname2 = hostname2;
+        this.serversocket1 = serversocket1;
+        this.port2 = port2;
+        this.serversocket2 = serversocket2;
+    }
+}
---
+++ ProjectIntelli/src/server/Client_Class.java
@@ -0,0 +1,22 @@
+package server;
+
+public class Client_Class {
+
+    public String username;
+    public String password;
+    public String department;
+    public String telephone;
+    public String address;
+    public String cc_expiration;
+    public String path;
+
+    public Client_Class(String username, String password, String path, String department, String telephone, String address, String cc_expiration){
+        this.username = username;
+        this.password = password;
+        this.path = path;
+        this.department = department;
+        this.telephone = telephone;
+        this.address = address;
+        this.cc_expiration = cc_expiration;
+    }
+}
